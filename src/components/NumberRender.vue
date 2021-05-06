a091a39b6d9ddaf7be79448ca5a80376997885e1
Hilton Meyer <development@hiltonmeyer.com>
2021-05-06 14:45:19 +0300

    feat(test): Computed Properties using call

---
+++ src/components/NumberRender.vue
@@ -0,0 +1,29 @@
+<template>{{ numbers }}</template>
+
+<script>
+export default {
+  props: {
+    even: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    numbers() {
+      const values = [];
+
+      for (let index = 1; index <= 10; index++) {
+        if (this.even) {
+          index % 2 === 0 ? values.push(index) : null;
+        } else {
+          index % 2 !== 0 ? values.push(index) : null;
+        }
+      }
+
+      return values.join(', ');
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/tests/NumberGenerator.spec.js
@@ -0,0 +1,20 @@
+import { mount } from '@vue/test-utils';
+import NumberRender from '../components/NumberRender.vue';
+
+const wrapper = mount(NumberRender, {
+  props: {
+    even: true,
+  },
+});
+
+describe('NumberRenderer', () => {
+  it('renders even numbers', () => {
+    expect(wrapper.text()).toBe('2, 4, 6, 8, 10');
+  });
+
+  it('renders odd numbers', () => {
+    const localThis = { even: false };
+
+    expect(NumberRender.computed.numbers.call(localThis)).toBe('1, 3, 5, 7, 9');
+  });
+});
