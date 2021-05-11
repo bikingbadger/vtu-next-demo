710c3f129a6f882e0feec77787c52385ccd29cc6
Hilton Meyer <development@hiltonmeyer.com>
2021-05-11 13:41:27 +0300

    feat(test): Simpulate user input

---
+++ src/components/FormSubmitter.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input data-username v-model="username" />
+    <input type="submit" />
+  </form>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            message: ''
+        }
+    },
+    methods: {
+        handleSubmit() {
+            this.message = `Thank you for your submission, ${this.username}`
+        }
+    }
+};
+</script>
+
+<style></style>
