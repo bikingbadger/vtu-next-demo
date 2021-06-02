aee714868396a2c88e2dbabe75ad647252b97514
Hilton Meyer <development@hiltonmeyer.com>
2021-06-02 09:48:20 +0300

    feat(test): Create user input test

--- src/components/FormSubmitter.vue
+++ src/components/FormSubmitter.vue
@@ -1,24 +1,43 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <input data-username v-model="username" />
+  <form @submit.prevent="handleSubmitAsync">
+    <input v-model="username" data-username />
     <input type="submit" />
   </form>
 
+  <div v-if="showMessage" class="message">
+    {{ message }}
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            username: '',
-            message: ''
-        }
+  data() {
+    return {
+      username: '',
+      showMessage: false,
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.showMessage = true;
     },
-    methods: {
-        handleSubmit() {
-            this.message = `Thank you for your submission, ${this.username}`
-        }
-    }
+    handleSubmitAsync() {
+        console.log(this);
+      return this.$http
+        .get('/api/v1/register', {})
+        .then(() => {
+          this.showMessage = true;
+        })
+        .catch((e) => {
+          throw Error('Something went wrong', e);
+        });
+    },
+  },
+  computed: {
+    message() {
+      return `Thank you for your submission, ${this.username}`;
+    },
+  },
 };
 </script>
 
--- src/tests/FormSubmitter.spec.js
+++ src/tests/FormSubmitter.spec.js
@@ -7,7 +7,6 @@ describe('FormSubmitter', () => {
 
     await wrapper.find('[data-username]').setValue('alice');
     await wrapper.find('form').trigger('submit.prevent');
-
     expect(wrapper.find('.message').text()).toBe(
       'Thank you for your submission, alice',
     );
